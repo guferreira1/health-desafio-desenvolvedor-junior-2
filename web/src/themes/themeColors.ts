4c22fa7fdd9c43a1d05b266fde204d1240605658
guferreira1 <gutoconsultor.imoveis@hotmail.com>
2022-12-15 21:44:36 -0300

    config: add colors

---
+++ web/src/themes/themeColors.ts
@@ -0,0 +1,12 @@
+export const Colors = {
+  primary: "#FE582C",
+  secundary: "#352455",
+  yellowMustard: "#EDB509",
+  yellow: "#FFF601",
+  white: "#FEFEFE",
+
+  feedback: {
+    success: "#3FE864",
+    negative: "#E83F5B",
+  },
+};
