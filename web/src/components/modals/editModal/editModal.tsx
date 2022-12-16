1170fb6eebabd967f4e5855ee9d56bede2e87f51
guferreira1 <gutoconsultor.imoveis@hotmail.com>
2022-12-16 12:53:44 -0300

    feat: add edit pet modal

--- web/src/components/modals/editModal/editModal.tsx
+++ web/src/components/modals/editModal/editModal.tsx
@@ -12,19 +12,24 @@ import {
   FormControl,
   FormLabel,
   Input,
-  Container,
 } from "@chakra-ui/react";
 
 import { PetsContext } from "../../../context/petsContext";
 import { Colors } from "../../../themes/themeColors";
+import { editPetInterface } from "../../../interfaces/petsInterface";
 
 export const EditModal = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
 
   const { isOpenEdit, onCloseEdit, petEdited } = useContext(PetsContext);
 
   const initialRef = useRef(null);
   const finalRef = useRef(null);
+
+  const onSubmit = (data: editPetInterface) => {
+    petEdited(data);
+    reset();
+  };
 
   return (
     <>
@@ -37,136 +42,127 @@ export const EditModal = () => {
         <ModalOverlay />
         <ModalContent
           maxW={550}
-          h="500px"
+          h="550px"
           mx={4}
           p={3}
           px={[0, 3]}
           border={`2px solid ${Colors.yellowMustard}`}
           bg={Colors.secundary}
-          alignSelf="center"
           borderRadius="5px"
         >
           <ModalHeader
             color={Colors.yellowMustard}
-            mt="9"
             textAlign="center"
             fontSize="20px"
           >
             Editar Pet
           </ModalHeader>
           <ModalCloseButton
-            mt={-10}
             mr={2}
             color={Colors.yellowMustard}
             alignSelf="flex-end"
           />
-          <Container w="90%" m="0 auto">
-            <form onSubmit={handleSubmit(petEdited)}>
-              <ModalBody pt={10} pb={6}>
-                <FormControl ml="25px">
-                  <FormLabel fontSize="18px" color={Colors.yellowMustard}>
-                    Nome
-                  </FormLabel>
-                  <Input
-                    placeholder="Digite o nome do pet"
-                    borderColor={Colors.yellowMustard}
-                    border="2px solid"
-                    borderRadius="5px"
-                    fontSize="14px"
-                    bg={Colors.secundary}
-                    height="40px"
-                    w="90%"
-                    color={Colors.white}
-                    padding="10px"
-                    {...register("name")}
-                  />
-                </FormControl>
 
-                <FormControl mt={4} ml="25px">
-                  <FormLabel fontSize="18px" color={Colors.yellowMustard}>
-                    Idade
-                  </FormLabel>
-                  <Input
-                    placeholder="Digite a idade do pet"
-                    borderColor={Colors.yellowMustard}
-                    border="2px solid"
-                    borderRadius="5px"
-                    fontSize="14px"
-                    bg={Colors.secundary}
-                    height="40px"
-                    w="90%"
-                    color={Colors.white}
-                    padding="10px"
-                    {...register("age")}
-                  />
-                </FormControl>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <ModalBody pt={4} pb={6}>
+              <FormControl>
+                <FormLabel fontSize="18px" color={Colors.yellowMustard}>
+                  Nome
+                </FormLabel>
+                <Input
+                  placeholder="Digite o nome do pet"
+                  borderColor={Colors.yellowMustard}
+                  border="2px solid"
+                  borderRadius="5px"
+                  fontSize="14px"
+                  bg={Colors.secundary}
+                  height="40px"
+                  w="90%"
+                  color={Colors.white}
+                  padding="10px"
+                  {...register("name")}
+                />
+              </FormControl>
 
-                <FormControl mt={4} ml="25px">
-                  <FormLabel fontSize="18px" color={Colors.yellowMustard}>
-                    Nome do dono
-                  </FormLabel>
-                  <Input
-                    placeholder="Digite o nome do dono do pet"
-                    borderColor={Colors.yellowMustard}
-                    border="2px solid"
-                    borderRadius="5px"
-                    fontSize="14px"
-                    bg={Colors.secundary}
-                    height="40px"
-                    w="90%"
-                    color={Colors.white}
-                    padding="10px"
-                    {...register("owner_name")}
-                  />
-                </FormControl>
+              <FormControl mt={4}>
+                <FormLabel fontSize="18px" color={Colors.yellowMustard}>
+                  Idade
+                </FormLabel>
+                <Input
+                  placeholder="Digite a idade do pet"
+                  borderColor={Colors.yellowMustard}
+                  border="2px solid"
+                  borderRadius="5px"
+                  fontSize="14px"
+                  bg={Colors.secundary}
+                  height="40px"
+                  w="90%"
+                  color={Colors.white}
+                  padding="10px"
+                  {...register("age")}
+                />
+              </FormControl>
 
-                <FormControl mt={4} ml="25px">
-                  <FormLabel fontSize="18px" color={Colors.yellowMustard}>
-                    Telefone do dono
-                  </FormLabel>
-                  <Input
-                    placeholder="Digite o telefone do dono do pet"
-                    borderColor={Colors.yellowMustard}
-                    border="2px solid"
-                    borderRadius="5px"
-                    fontSize="14px"
-                    bg={Colors.secundary}
-                    height="40px"
-                    w="90%"
-                    color={Colors.white}
-                    padding="10px"
-                    {...register("owner_phone")}
-                  />
-                </FormControl>
-              </ModalBody>
+              <FormControl mt={4}>
+                <FormLabel fontSize="18px" color={Colors.yellowMustard}>
+                  Nome do dono
+                </FormLabel>
+                <Input
+                  placeholder="Digite o nome do dono do pet"
+                  borderColor={Colors.yellowMustard}
+                  border="2px solid"
+                  borderRadius="5px"
+                  fontSize="14px"
+                  bg={Colors.secundary}
+                  height="40px"
+                  w="90%"
+                  color={Colors.white}
+                  padding="10px"
+                  {...register("owner_name")}
+                />
+              </FormControl>
 
-              <ModalFooter
-                w="100%"
-                display="flex"
-                justifyContent="space-evenly"
-                mt="20px"
+              <FormControl mt={4}>
+                <FormLabel fontSize="18px" color={Colors.yellowMustard}>
+                  Telefone do dono
+                </FormLabel>
+                <Input
+                  placeholder="Digite o telefone do dono do pet"
+                  borderColor={Colors.yellowMustard}
+                  border="2px solid"
+                  borderRadius="5px"
+                  fontSize="14px"
+                  bg={Colors.secundary}
+                  height="40px"
+                  w="90%"
+                  color={Colors.white}
+                  padding="10px"
+                  {...register("owner_phone")}
+                />
+              </FormControl>
+            </ModalBody>
+
+            <ModalFooter w="100%" display="flex" justifyContent="space-evenly">
+              <Button
+                type="submit"
+                bg={Colors.yellowMustard}
+                w="100px"
+                borderRadius="5px"
+                color={Colors.secundary}
               >
-                <Button
-                  type="submit"
-                  bg={Colors.yellowMustard}
-                  w="100px"
-                  borderRadius="5px"
-                  color={Colors.secundary}
-                >
-                  Editar
-                </Button>
-                <Button
-                  onClick={onCloseEdit}
-                  bg={Colors.yellowMustard}
-                  w="100px"
-                  borderRadius="5px"
-                  color={Colors.secundary}
-                >
-                  Cancelar
-                </Button>
-              </ModalFooter>
-            </form>
-          </Container>
+                Editar
+              </Button>
+              <Button
+                onClick={onCloseEdit}
+                bg={Colors.yellowMustard}
+                w="100px"
+                borderRadius="5px"
+                color={Colors.secundary}
+              >
+                Cancelar
+              </Button>
+            </ModalFooter>
+          </form>
         </ModalContent>
       </Modal>
     </>
